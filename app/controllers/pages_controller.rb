c0ee5a821e09ccc88048efed3761dc3c7ca2b985
Nomi Vos <nvoswork@gmail.com>
2016-09-09 17:42:56 -0500

    partial changes to styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,6 @@ class PagesController < ApplicationController
   def about
   end
 
-  def contact
+  def blog
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'pages#about', as: :about
 
-  get 'contact' => 'pages#contact', as: :contact
+  get 'blog' => 'pages#blog', as: :blog
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
